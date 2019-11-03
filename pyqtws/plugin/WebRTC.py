6f20484ff2da54cfb9150810e96c089d8dd684cd
Simone Scalabrino <s.scalabrino9@gmail.com>
2019-11-03 19:03:52 +0100

    WebRTC support (working, but not with Skype)

--- pyqtws/plugin/WebRTC.py
+++ pyqtws/plugin/WebRTC.py
@@ -5,14 +5,16 @@ from web import QTWSWebView
 from config import QTWSConfig
 
 
-class VoiceVideo(QTWSPlugin):    
+class WebRTC(QTWSPlugin):    
     def __init__(self, config: QTWSConfig):
-        super().__init__("VoiceVideo")
+        super().__init__("WebRTC")
 
     def web_engine_setup(self, web: QTWSWebView):
         self.web = web
         self.web.grant_permission(QWebEnginePage.MediaAudioVideoCapture)
+        self.web.grant_permission(QWebEnginePage.MediaAudioCapture)
+        self.web.grant_permission(QWebEnginePage.MediaVideoCapture)
         self.web.grant_permission(QWebEnginePage.DesktopAudioVideoCapture)
     
 def instance(config: QTWSConfig, params: dict):
-    return VoiceVideo(config)
+    return WebRTC(config)
