e587e21407755d44f54686632b5723d6f62280f7
Simone Scalabrino <s.scalabrino9@gmail.com>
2020-11-16 09:48:21 +0100

    Simplified setup

--- setup.py
+++ setup.py
@@ -6,48 +6,8 @@ import glob
 
 from pyqtws.config import QTWSConfig
 
-SRCDIR = "pyqtws"
-HOME_PATH = os.path.dirname(os.path.realpath(__file__)) + "/" + SRCDIR
-
-
-def make_desktop_file(app_src, desktop_dest):
-    print("Building desktop file for " + app_src)
-
-    config = QTWSConfig(app_src)
-
-    script_path = "silo -a " + os.path.basename(app_src).replace(".qtws", "")
-
-    content = "[Desktop Entry]\n" \
-              "Categories=Internet\n" \
-              "Comment=" + config.description + "\n" \
-              "Exec=" + script_path + "\n" \
-              "GenericName=" + config.name + "\n" \
-              "Icon=silo-" + os.path.splitext(os.path.basename(config.icon))[0] + "\n" \
-              "MimeType=\n" \
-              "Name=" + config.name + "\n" \
-              "Path=\n" \
-              "StartupNotify=true\n" \
-              "Terminal=false\n" \
-              "Type=Application"
-    with open(desktop_dest, "w") as f:
-        f.write(content)
-
-
-for app in glob.glob("pyqtws/apps/*.qtws"):
-    app_base_name = app.replace("pyqtws/apps/", "").replace(".qtws", "")
-    desktop_file = os.path.join(HOME_PATH, "desktops/" + app_base_name + ".desktop")
-    make_desktop_file(app, desktop_file)
-
-
-package_data = {
-    "pyqtws.apps": list(map(lambda f: os.path.basename(f), glob.glob(SRCDIR + "/apps/*.qtws"))),
-    "pyqtws.apps.appChooser": list(map(lambda f: os.path.basename(f), glob.glob(SRCDIR + "/apps/appChooser*.qtws"))),
-    "pyqtws.icons": list(map(lambda f: os.path.basename(f), glob.glob(SRCDIR + "/icons/*"))),
-    "pyqtws.desktops": list(map(lambda f: os.path.basename(f), glob.glob(SRCDIR + "/desktops/*")))
-}
-
-setuptools.setup(name='silo',
-                 version='0.1',
+setuptools.setup(name='pyqtws',
+                 version='0.1.47',
                  scripts=["silo"],
                  description='Standalone website wrapper',
                  url='http://github.com/intersimone999/pyqtws',
