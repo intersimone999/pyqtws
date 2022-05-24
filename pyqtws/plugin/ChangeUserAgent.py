c156ddc9d526be0aad286eddff9be52ab8586203
Simone Scalabrino <s.scalabrino9@gmail.com>
2022-05-24 19:26:43 +0200

    Updated user agent strings

--- pyqtws/plugin/ChangeUserAgent.py
+++ pyqtws/plugin/ChangeUserAgent.py
@@ -11,10 +11,11 @@ class ChangeUserAgent(QTWSPlugin):
         
         self.__default_user_agents["chrome"] = "Mozilla/5.0 " \
             "(Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " \
-            "(KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36"
+            "(KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36"
         
         self.__default_user_agents["firefox"] = "Mozilla/5.0 " \
-            "(Windows NT 5.1; rv:7.0.1) Gecko/20100101 Firefox/7.0.1"
+            "(X11; Linux x86_64; rv:100.0) " \
+            "Gecko/20100101 Firefox/100.0"
 
         if user_agent in self.__default_user_agents.keys():
             self.user_agent = self.__default_user_agents[user_agent]
