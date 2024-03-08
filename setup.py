e25c1b84dd34aa233bf32b4b2c3ed2e25a4e36ad
Simone Scalabrino <s.scalabrino9@gmail.com>
2024-03-08 18:27:43 +0100

    Bumped to version 0.1.61

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 import setuptools
 
 setuptools.setup(name='pyqtws',
-                 version='0.1.60',
+                 version='0.1.61',
                  scripts=["silo"],
                  description='Standalone website wrapper',
                  url='http://github.com/intersimone999/pyqtws',
