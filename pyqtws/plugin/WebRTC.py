d00db8a84059f649e35e87bc747055067a3e1f1c
Simone Scalabrino <s.scalabrino9@gmail.com>
2024-04-02 23:10:41 +0200

    Fixed screen sharing with Qt6
    Probably it did not work with Qt5 for the same reason

--- pyqtws/plugin/WebRTC.py
+++ pyqtws/plugin/WebRTC.py
@@ -1,4 +1,8 @@
 from PyQt6.QtWebEngineCore import QWebEnginePage
+from PyQt6.QtWebEngineCore import QWebEngineUrlRequestInterceptor
+from PyQt6.QtWebEngineCore import QWebEngineUrlRequestInfo
+from PyQt6.QtWebEngineCore import QWebEngineProfile
+from PyQt6.QtWebEngineCore import QWebEngineSettings
 
 from plugins import QTWSPlugin
 from web import QTWSWebView
@@ -14,7 +18,12 @@ class WebRTC(QTWSPlugin):
         self.web.grant_permission(QWebEnginePage.Feature.MediaAudioVideoCapture)
         self.web.grant_permission(QWebEnginePage.Feature.MediaAudioCapture)
         self.web.grant_permission(QWebEnginePage.Feature.MediaVideoCapture)
+        self.web.grant_permission(QWebEnginePage.Feature.DesktopVideoCapture)
         self.web.grant_permission(QWebEnginePage.Feature.DesktopAudioVideoCapture)
+        self.web.settings().setAttribute(
+            QWebEngineSettings.WebAttribute.ScreenCaptureEnabled, 
+            True
+        )
     
 
 def instance(config: QTWSConfig, params: dict):
